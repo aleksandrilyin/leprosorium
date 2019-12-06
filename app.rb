e743dd1e8b355253c45ed384410fd871e78fe3f7
Alexander Ilyin <alexander.ilyin11@gmail.com>
2019-12-06 03:26:10 +0300

    Add parameter validation

--- app.rb
+++ app.rb
@@ -33,6 +33,11 @@ end
 
 post '/new' do
 	content = params[:content]
-	
-	erb "You typed #{content}"
+
+	if content.length <= 0
+		@error = 'Type post text'
+		return erb :new
+	end
+
+	erb "You typed: #{content}"
 end
